d12672b3b101d0d41a682e82de182041fd844659
varinder-singh1 <varindersingh43258@gmail.com>
2025-03-01 10:12:13 +0530

    set api link in env

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,5 @@
 export async function fetchPokemonDetail(id: string) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_ADDRESS}/${id}`);
     return res.json();
   }
   
@@ -10,7 +10,7 @@ export async function fetchPokemonDetail(id: string) {
 }
 
  export async function fetchPokemons(): Promise<Pokemon[]> {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_ADDRESS}?limit=50`);
     const data = await res.json();
     return data.results.map((p: any, index: number) => ({
       ...p,
